b56f4a7301a75df42e7b92b8b05beba1562ee15a
John Spinella <jrspinella@users.noreply.github.com>
2023-04-11 14:04:26 -0400

    move bastion to shared services

--- src/modules/shared_services/variables.tf
+++ src/modules/shared_services/variables.tf
@@ -1,18 +1,2 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
-
-variable "service_alerts" {
-  type = list(object({
-    alert_name        = string
-    alert_scope       = string
-    alert_description = string
-    alert_severity    = string
-    alert_status      = string
-    alert_impact      = string
-    alert_service     = string
-    alert_region      = string
-    alert_start_time  = string
-    alert_end_time    = string
-  }))
-  default = []
-}
