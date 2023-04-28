4142847fb95c49126cb2c0f60becf05f345135bc
Curtis Slone <curtisdslone@gmail.com>
2023-04-28 14:14:37 +0200

    Updating alerts

--- src/modules/sentinel/resource.sentinalalerts.tf
+++ src/modules/sentinel/resource.sentinalalerts.tf
@@ -7,7 +7,18 @@ DESCRIPTION: This resource generates sentinel alerts based on the alert rules in
 AUTHOR/S: Curtis Slone
 */
 
-# resource "azurerm_sentinel_alert_rule_scheduled" "sentinel_alert" {
-#     for_each = var.sentinel_rule_alerts
+resource "azurerm_sentinel_alert_rule_scheduled" "sentinel_alert" {
     
-# }
+    for_each = local.alert_rules
+    name = each.key
+    log_analytics_workspace_id = var.log_analytics_ws_id
+    query_frequency = each.value.query_frequency
+    query_period = each.value.query_period
+    severity = each.value.severity
+    query = each.value.query
+
+    # https://faultbucket.ca/2020/09/terraform-nested-for_each-example/
+    entity_mapping {
+      mappings = {}
+    }
+}
